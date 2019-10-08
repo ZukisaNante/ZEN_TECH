41dada9a17a69998740812cd6874e4a9f1d04245
junior <zukisanante@gmail.com>
2019-10-08 13:44:23 +0200

    js maps created

---
+++ front_end/js/maps.js
@@ -0,0 +1,8 @@
+/*--##################################################### 
+#   ZEN_TECH solutions                                  #
+#   Programmer: Zukisa NANTE                            #
+#   Qualifications: Bachelor Electrical  Engineering    #
+#   Specialisation: Computer Operating Systems          #
+######################################################-*/
+
+/* function to call my API using my key */
